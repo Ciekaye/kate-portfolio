d305be94abca7a7f81635c7891c46c43639c4699
Kate Cristyl Osorio <clarkuyanguren@gmail.com>
2026-03-16 01:12:25 +0800

    chore: setup vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
 import '@/styles/globals.css'
 import { motion } from 'framer-motion'
+import { Analytics } from "@vercel/analytics/next"
 
 function MyApp({ Component, pageProps }) {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-    >
-      <Component {...pageProps} />
-    </motion.div>
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+      <Analytics />
+    </>
   )
 }
 
